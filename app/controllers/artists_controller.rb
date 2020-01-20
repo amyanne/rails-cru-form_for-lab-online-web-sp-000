9c4b1d8b662a53c6619e7d1a74745e15ee8a7b6a
Amy Westcott <amy.westcott@yahoo.com>
2020-01-20 20:22:22 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -25,4 +25,13 @@ class ArtistsController < ApplicationController
       @artist.update(artist_params)
       redirect_to artist_path(@artist)
    end
+   
+   private
+      def artist_params
+         params.require(:artist).permit(:name, :bio)        
+      end
+
+      def set_artist
+         @artist = Artist.find_by_id(params[:id])
+      end
 end
