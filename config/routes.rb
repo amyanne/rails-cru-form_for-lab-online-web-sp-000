25fe976a741d8c8226edfb4ca6a1c2ce1af34ee4
Amy Westcott <amy.westcott@yahoo.com>
2020-01-20 20:18:42 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,7 @@
 Rails.application.routes.draw do
-  get 'genres/index'
-
-  get 'genres/new'
-
-  get 'genres/create'
-
-  get 'genres/show'
-
-  get 'genres/edit'
-
-  get 'genres/update'
-
-  get 'songs/index'
-
-  get 'songs/new'
-
-  get 'songs/create'
-
-  get 'songs/show'
-
-  get 'songs/edit'
-
-  get 'songs/update'
-
-  get 'artists/index'
-
-  get 'artists/new'
-
-  get 'artists/create'
-
-  get 'artists/show'
-
-  get 'artists/edit'
-
-  get 'artists/update'
+  resources :artists, except: :delete
+  resources :genres, except: :delete
+  resources :songs, except: :delete
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
